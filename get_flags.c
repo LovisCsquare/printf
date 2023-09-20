fa0b70e2209a93c9684c66116a94b4d6f9f2b91f
LovisCsquare <clovisfuh1996@gmail.com>
2023-09-20 03:45:13 +0100

    printf

--- get_flags.c
+++ get_flags.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * get_flags - Calculates active flags
  * @format: Formatted string in which to print the arguments
@@ -9,7 +10,6 @@ int get_flags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
-
 	int j, curr_i;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
@@ -26,6 +26,8 @@ int get_flags(const char *format, int *i)
 		if (FLAGS_CH[j] == 0)
 			break;
 	}
+
 	*i = curr_i - 1;
+
 	return (flags);
 }
